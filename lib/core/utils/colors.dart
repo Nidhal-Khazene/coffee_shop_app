fe344811b822fcb1152b55c32a3392be2696facd
nidhal khazene <nidhal.khazene@gmail.com>
2026-04-20 18:03:02 +0100

    add assets and core folders

---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorsData {
+  Color kPrimary = const Color(0xFFC67C4E);
+
+  Color kFontPrimary = const Color(0xFF242424);
+  Color kFontSecondary = const Color(0xFF303030);
+  Color kFontThird = const Color(0xFFA2A2A2);
+}
