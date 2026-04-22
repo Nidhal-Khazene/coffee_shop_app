aca90981130790124071c500c5438c3333a37931
nidhal khazene <nidhal.khazene@gmail.com>
2026-04-22 10:32:03 +0100

    on boarding view done

--- lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
@@ -1,3 +1,8 @@
+import 'package:coffee_shop_app/core/constants/constants.dart';
+import 'package:coffee_shop_app/core/utils/assets.dart';
+import 'package:coffee_shop_app/core/utils/colors.dart';
+import 'package:coffee_shop_app/core/utils/styles.dart';
+import 'package:coffee_shop_app/core/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 
 class OnBoardingViewBody extends StatelessWidget {
@@ -5,6 +10,52 @@ class OnBoardingViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Stack(
+      children: [
+        SizedBox(
+          height: double.infinity,
+          width: double.infinity,
+          child: Positioned.fill(
+            child: Image.asset(Assets.imagesOnBoardingBackgroundImage),
+          ),
+        ),
+        Positioned(
+          child: Padding(
+            padding: kPrimaryPadding,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  child: Center(
+                    child: Text(
+                      "Fall in Love with Coffee in Blissful Delight!",
+                      textAlign: TextAlign.center,
+                      style: AppStyles.semiBold32.copyWith(color: Colors.white),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 8),
+                SizedBox(
+                  width: double.infinity,
+                  child: Center(
+                    child: Text(
+                      "Welcome to our cozy coffee corner, where every cup is a delightful for you.",
+                      textAlign: TextAlign.center,
+                      style: AppStyles.regular14.copyWith(
+                        color: ColorsData.kFontThird,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 32),
+                CustomButton(content: 'Get Started'),
+                const SizedBox(height: 64),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
