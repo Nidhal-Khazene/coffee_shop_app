c67e19abd855b7b3c6419dfe63eab79b23cb6650
nidhal khazene <nidhal.khazene@gmail.com>
2026-04-22 10:31:09 +0100

    add custom button

---
+++ lib/core/widgets/custom_button.dart
@@ -0,0 +1,40 @@
+import 'package:coffee_shop_app/core/utils/colors.dart';
+import 'package:coffee_shop_app/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    super.key,
+    required this.content,
+    this.onTap,
+    this.bgColor,
+    this.textStyle,
+  });
+  final String content;
+  final void Function()? onTap;
+  final Color? bgColor;
+  final TextStyle? textStyle;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+        decoration: ShapeDecoration(
+          color: bgColor ?? ColorsData.kPrimary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+        ),
+        child: Center(
+          child: Text(
+            content,
+            style:
+                textStyle ?? AppStyles.semiBold16.copyWith(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
