480a56b941496af5b5f4b8066c961c689c359c4c
nidhal khazene <nidhal.khazene@gmail.com>
2026-04-22 10:52:08 +0100

    add some assets

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -2,6 +2,28 @@
 class Assets {
   Assets._();
   
+  static const String imagesCoffee1 = "assets/images/coffee_1.png";
+
+  static const String imagesCoffee2 = "assets/images/coffee_2.png";
+
+  static const String imagesCoffee3 = "assets/images/coffee_3.png";
+
+  static const String imagesCoffee4 = "assets/images/coffee_4.png";
+
+  static const String imagesCoffeeDetails = "assets/images/coffee_details.png";
+
+  static const String imagesHelperCoffeeBeansIcon = "assets/images/helper/coffee_beans_icon.svg";
+
+  static const String imagesHelperCoffeePackageIcon = "assets/images/helper/coffee_package_icon.svg";
+
+  static const String imagesHelperMotorBikeIcon = "assets/images/helper/motor_bike_icon.svg";
+
+  static const String imagesHomeBannerPromo = "assets/images/home_banner_promo.png";
+
   static const String imagesOnBoardingBackgroundImage = "assets/images/on_boarding_background_image.png";
+
+  static const String imagesPerson = "assets/images/person.png";
+
+  static const String imagesWhitedBlackBg = "assets/images/whited_black_bg.png";
 }
 
