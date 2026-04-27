20913e325b9f579f02f219371bac2514a6762a1b
nidhal khazene <nidhal.khazene@gmail.com>
2026-04-27 10:26:38 +0100

    coffee increment decrement

---
+++ lib/features/order/presentation/views/widgets/coffee_increment_decrement.dart
@@ -0,0 +1,94 @@
+import 'package:coffee_shop_app/core/utils/assets.dart';
+import 'package:coffee_shop_app/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class CoffeeIncrementDecrement extends StatefulWidget {
+  const CoffeeIncrementDecrement({super.key});
+
+  @override
+  State<CoffeeIncrementDecrement> createState() =>
+      _CoffeeIncrementDecrementState();
+}
+
+class _CoffeeIncrementDecrementState extends State<CoffeeIncrementDecrement> {
+  int quantity = 1;
+
+  void increment() {
+    setState(() => quantity++);
+  }
+
+  void decrement() {
+    if (quantity > 1) {
+      setState(() => quantity--);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Row(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Image.asset(Assets.imagesCoffee1, height: 54, width: 54),
+              ),
+              const SizedBox(width: 16),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Caffe Mocha', style: AppStyles.semiBold16),
+                  const SizedBox(height: 4),
+                  Text(
+                    'Deep Foam',
+                    style: AppStyles.regular12.copyWith(
+                      color: const Color(0xFFA2A2A2),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+
+        /// RIGHT SIDE (Counter)
+        Row(
+          children: [
+            _buildButton(icon: Icons.remove, onTap: decrement),
+
+            AnimatedSwitcher(
+              duration: const Duration(milliseconds: 200),
+              transitionBuilder: (child, animation) =>
+                  ScaleTransition(scale: animation, child: child),
+              child: Text(
+                '$quantity',
+                key: ValueKey(quantity),
+                style: AppStyles.semiBold14,
+              ),
+            ),
+
+            _buildButton(icon: Icons.add, onTap: increment),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildButton({required IconData icon, required VoidCallback onTap}) {
+    return GestureDetector(
+      onTap: onTap,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 200),
+        margin: const EdgeInsets.symmetric(horizontal: 8),
+        padding: const EdgeInsets.all(6),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: const Color(0xFFF9F2ED)),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Icon(icon, size: 16, color: Colors.black),
+      ),
+    );
+  }
+}
--- lib/features/order/presentation/views/widgets/order_view_body.dart
+++ lib/features/order/presentation/views/widgets/order_view_body.dart
@@ -1,7 +1,9 @@
 import 'package:coffee_shop_app/core/constants/constants.dart';
+import 'package:coffee_shop_app/features/order/presentation/views/widgets/coffee_increment_decrement.dart';
 import 'package:coffee_shop_app/features/order/presentation/views/widgets/order_delivery_address.dart';
 import 'package:coffee_shop_app/features/order/presentation/views/widgets/order_switch_deliver_pick_up.dart';
 import 'package:coffee_shop_app/features/order/presentation/views/widgets/order_view_custom_app_bar.dart';
+import 'package:coffee_shop_app/shared/widgets/custom_divider.dart';
 import 'package:flutter/material.dart';
 
 class OrderViewBody extends StatelessWidget {
@@ -9,18 +11,30 @@ class OrderViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(
-      child: Padding(
-        padding: kPrimaryPadding,
-        child: Column(
-          children: [
-            OrderViewCustomAppBar(),
-            SizedBox(height: 24),
-            OrderSwitchDeliverPickUp(),
-            SizedBox(height: 24),
-            OrderDeliveryAddress(),
-          ],
-        ),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          const Padding(
+            padding: kPrimaryPadding,
+            child: Column(
+              children: [
+                OrderViewCustomAppBar(),
+                SizedBox(height: 24),
+                OrderSwitchDeliverPickUp(),
+                SizedBox(height: 24),
+                OrderDeliveryAddress(),
+                CustomDivider(),
+                CoffeeIncrementDecrement(),
+                SizedBox(height: 16),
+              ],
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            height: 4,
+            decoration: const BoxDecoration(color: Color(0xFFF9F2ED)),
+          ),
+        ],
       ),
     );
   }
