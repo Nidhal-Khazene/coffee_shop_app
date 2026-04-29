ea525ceebc935c1cf25311286b8ef521dd019757
nidhal khazene <nidhal.khazene@gmail.com>
2026-04-29 08:38:31 +0100

    fix the arrow back icon in all the custom app bars

--- lib/features/home/presentation/views/widgets/coffee_details_custom_app_bar.dart
+++ lib/features/home/presentation/views/widgets/coffee_details_custom_app_bar.dart
@@ -1,6 +1,7 @@
 import 'package:coffee_shop_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:iconly/iconly.dart';
+import 'package:iconsax/iconsax.dart';
 
 class CoffeeDetailsCustomAppBar extends StatefulWidget {
   const CoffeeDetailsCustomAppBar({super.key});
@@ -21,7 +22,7 @@ class _CoffeeDetailsCustomAppBarState extends State<CoffeeDetailsCustomAppBar> {
           onTap: () {
             Navigator.pop(context);
           },
-          child: const Icon(Icons.arrow_back_ios_new_outlined, size: 24),
+          child: const Icon(Iconsax.arrow_left_2),
         ),
         Text("Detail", style: AppStyles.semiBold16),
         GestureDetector(
--- lib/features/order/presentation/views/widgets/order_view_custom_app_bar.dart
+++ lib/features/order/presentation/views/widgets/order_view_custom_app_bar.dart
@@ -1,5 +1,6 @@
 import 'package:coffee_shop_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class OrderViewCustomAppBar extends StatelessWidget {
   const OrderViewCustomAppBar({super.key});
@@ -13,7 +14,7 @@ class OrderViewCustomAppBar extends StatelessWidget {
           onTap: () {
             Navigator.pop(context);
           },
-          child: const Icon(Icons.arrow_back_ios_new_outlined, size: 24),
+          child: const Icon(Iconsax.arrow_left_2),
         ),
         Text("Order", style: AppStyles.semiBold16),
         const SizedBox(),
